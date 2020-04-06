096e6e61c957efd4bbb937f8c05b66fa954c60e0
Marius Rochon <mrochon@microsoft.com>
2020-04-05 21:06:33 -0700

    Initial commit

---
+++ B2CMultiTenant/Models/Invitee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace B2CMultiTenant.Models
+{
+    public class Invitee
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; }
+        public string InvitationUrl { get; set; }
+    }
+}
